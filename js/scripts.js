dc4ae3af7a59b82d1d46c91971ed6deea7845a58
austin cummings and rebecca mcintosh <rebecca@>
2017-08-08 15:30:44 -0700

    added buttons, js, and scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+  $("button#bw").click(function() {
+    $("body").removeClass();
+    $("body").addClass("bw-background");
+  });
+  $("button#original").click(function() {
+  $("body").removeClass();
+  $("body").addClass("original-background");
+});
+});
